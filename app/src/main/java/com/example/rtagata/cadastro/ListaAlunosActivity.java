5af855555259d31260d56bf54ff6eedd38a95db5
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-08-18 16:14:03 -0300

    2.1 - Criando o Projeto
    Mudança de Layout
    Criando uma lista de Array
    Lendo a lista de Array

--- app/src/main/java/com/example/rtagata/cadastro/ListaAlunosActivity.java
+++ app/src/main/java/com/example/rtagata/cadastro/ListaAlunosActivity.java
@@ -2,12 +2,22 @@ package com.example.rtagata.cadastro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
+
+    private ListView listaAlunos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Auei", "Buiei", "Cuei"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        listaAlunos.setAdapter(adapter);
+
     }
 }
