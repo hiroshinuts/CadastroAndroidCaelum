e48739902d3983b24c3003fd0159f5da4f6b2f10
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-08-19 11:26:35 -0300

    3.9 - Começando o Menu
    Criando float Botton no menu
    Action Bar
    
    4.2 - Nossa Classe de Modelo e Extraindo os Dados do Formulario
    
    Criando a entidade

---
+++ app/src/main/java/com/example/rtagata/cadastro/FormularioActivity.java
@@ -0,0 +1,43 @@
+package com.example.rtagata.cadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        this.helper = new FormularioHelper(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_formulario_ok:
+                Toast.makeText(this, helper.pegaAlunoDoFormulario().getNome().toString(), Toast.LENGTH_LONG).show();
+                finish();
+                return false;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
