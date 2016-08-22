7ce951825f65faa92c70cda23631eb8a7971bf84
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2016-08-22 15:42:47 -0300

    7.4 -  Tirando fotos Usando a Camera
    Habilitando Camera,
    Gravando foto no banco para exibicao no Formulario

--- app/src/main/java/com/example/rtagata/cadastro/FormularioActivity.java
+++ app/src/main/java/com/example/rtagata/cadastro/FormularioActivity.java
@@ -1,6 +1,9 @@
 package com.example.rtagata.cadastro;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,18 +16,19 @@ import com.example.rtagata.cadastro.dao.AlunoDAO;
 import com.example.rtagata.cadastro.extras.Extras;
 import com.example.rtagata.cadastro.modelo.Aluno;
 
+import java.io.File;
+
 public class FormularioActivity extends AppCompatActivity {
 
     private FormularioHelper helper;
     public static final String ALUNO_SELECIONADO = "alunoSelecionado";
+    private String localArquivoFoto;
+    private static final int TIRA_FOTO = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-
-
 
         Intent intent = getIntent();
         final Aluno alunoParaSerAlterado = (Aluno) intent.getSerializableExtra(Extras.ALUNO_SELECIONADO);
@@ -36,7 +40,31 @@ public class FormularioActivity extends AppCompatActivity {
             helper.colocaNoFormulario(alunoParaSerAlterado);
         }
 
+        Button foto = helper.getFotoButton();
+        foto.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                localArquivoFoto = getExternalFilesDir(null) + "/" + System.currentTimeMillis()+".jpg";
+                Intent irParaCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Uri localFoto = Uri.fromFile(new File(localArquivoFoto));
+                irParaCamera.putExtra(MediaStore.EXTRA_OUTPUT, localFoto);
+                startActivityForResult(irParaCamera, TIRA_FOTO);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == TIRA_FOTO){
+            if(resultCode == Activity.RESULT_OK){
+                helper.carregaImagem(this.localArquivoFoto);
+            }else{
+                this.localArquivoFoto = null;
+            }
+        }
     }
 
     @Override
